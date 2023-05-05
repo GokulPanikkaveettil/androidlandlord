85c247350a64370ed128e65507773503db633fde
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2023-05-05 21:40:44 +0100

    removed print statement

--- app/src/main/java/com/example/voyagerdiaries/DbHelper.kt
+++ app/src/main/java/com/example/voyagerdiaries/DbHelper.kt
@@ -15,7 +15,6 @@ class Database {
     }
 
     private fun connect() {
-        println("hello.....")
         val thread = Thread {
             try {
                 Class.forName("org.postgresql.Driver")

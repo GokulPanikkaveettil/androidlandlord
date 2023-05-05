c69a6d0d66a3faddc17cf5f1e01cb08bfdb568c2
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2023-05-05 21:42:09 +0100

    removed function getUsers

--- app/src/main/java/com/example/voyagerdiaries/DbHelper.kt
+++ app/src/main/java/com/example/voyagerdiaries/DbHelper.kt
@@ -36,32 +36,6 @@ class Database {
             status = false
         }
     }
-    fun printUsersTable() {
-        val thread = Thread {
-            val query = "SELECT * FROM users"
-            try {
-                val statement = connection?.createStatement()
-                val resultSet = statement?.executeQuery(query)
-                while (resultSet?.next() == true) {
-                    val id = resultSet.getInt("id")
-                    val firstName = resultSet.getString("first_name")
-                    val lastName = resultSet.getString("last_name")
-                    val username = resultSet.getString("username")
-                    val password = resultSet.getString("password")
-                    println("$id, $firstName, $lastName, $username, $password")
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        thread.start()
-        try {
-            thread.join()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            status = false
-        }
-    }
 
     fun addNewUser(firstName: String, lastName: String, userName: String, password: String): Boolean {
         val thread = Thread {

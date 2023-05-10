b828cb9a1c7f1db109f880255a6c3f4163b36bc3
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2023-05-10 07:38:44 +0100

    added two buttons in profile view

--- app/src/main/java/com/example/voyagerdiaries/Profile.kt
+++ app/src/main/java/com/example/voyagerdiaries/Profile.kt
@@ -1,7 +1,9 @@
 package com.example.voyagerdiaries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Profile : AppCompatActivity() {
@@ -13,5 +15,15 @@ class Profile : AppCompatActivity() {
         navbarActions(this, nav);
         val selectedItem = nav.menu.findItem(R.id.navbar_profile)
         selectedItem?.setChecked(true)
+
+
+        val profileUpdateView = findViewById<ImageButton>(R.id.profileUpdateView)
+        profileUpdateView.setOnClickListener{
+            startActivity(Intent(this, ProfileUpdate::class.java))
+        }
+        val myReviewsList = findViewById<ImageButton>(R.id.myReviewsList)
+        myReviewsList.setOnClickListener{
+            startActivity(Intent(this, MyReviews::class.java))
+        }
     }
 }

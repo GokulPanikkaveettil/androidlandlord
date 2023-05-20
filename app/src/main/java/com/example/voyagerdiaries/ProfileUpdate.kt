3e2598eb0ea5ab599ffd765e387324b661c8414c
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2023-05-20 21:18:20 +0100

    issue fixed

--- app/src/main/java/com/example/voyagerdiaries/ProfileUpdate.kt
+++ app/src/main/java/com/example/voyagerdiaries/ProfileUpdate.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.*
 
@@ -36,10 +37,16 @@ class ProfileUpdate : AppCompatActivity() {
         updateLastName.setText(lastName);
         val updateButton = findViewById<Button>(R.id.updateProfileButton);
         updateButton.setOnClickListener {
-            coroutineScope.launch {
-                updateProfile(updateFirstName.text.toString(), updateLastName.text.toString());
-                val intent = Intent(this@ProfileUpdate, Reviews::class.java)
-                startActivity(intent)
+            if(updateFirstName.text.isBlank() or updateLastName.text.isBlank()){
+                Toast.makeText(this@ProfileUpdate, "firstname and lastname cannot be blank", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            else {
+                coroutineScope.launch {
+                    updateProfile(updateFirstName.text.toString(), updateLastName.text.toString());
+                    val intent = Intent(this@ProfileUpdate, Reviews::class.java)
+                    startActivity(intent)
+                }
             }
         }
 
